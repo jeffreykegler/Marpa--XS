ad54cb4d36b947a17938480982fedeb288caa3f3
Jeffrey Kegler <jeffreykegler@Jeffrey-Keglers-Computer.local>
2011-11-01 10:07:27 -0700

    CPAN XS-0.020000

--- xs/lib/Marpa/XS.pm
+++ xs/lib/Marpa/XS.pm
@@ -20,7 +20,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION $STRING_VERSION @ISA $DEBUG);
-$VERSION        = '0.019_004';
+$VERSION        = '0.020000';
 $STRING_VERSION = $VERSION;
 ## no critic (BuiltinFunctions::ProhibitStringyEval)
 $VERSION = eval $VERSION;
