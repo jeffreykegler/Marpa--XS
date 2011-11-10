29d0ea6cfbf84081adc0c8a0ed51553e6c471109
Jeffrey Kegler <jeffreykegler@Jeffrey-Keglers-Computer.local>
2011-11-10 11:55:33 -0800

    Bumped xs version.  t+

--- xs/lib/Marpa/XS.pm
+++ xs/lib/Marpa/XS.pm
@@ -20,7 +20,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION $STRING_VERSION @ISA $DEBUG);
-$VERSION        = '0.020000';
+$VERSION        = '0.021_000';
 $STRING_VERSION = $VERSION;
 ## no critic (BuiltinFunctions::ProhibitStringyEval)
 $VERSION = eval $VERSION;
