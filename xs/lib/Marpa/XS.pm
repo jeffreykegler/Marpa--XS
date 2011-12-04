0a8e63218f7c961e641a291c766bee8d53cb20d7
Jeffrey Kegler <jeffreykegler@Jeffrey-Keglers-Computer.local>
2011-12-04 09:29:33 -0800

    CPAN XS-0.025_001

--- xs/lib/Marpa/XS.pm
+++ xs/lib/Marpa/XS.pm
@@ -20,7 +20,7 @@ use strict;
 use warnings;
 
 use vars qw($VERSION $STRING_VERSION @ISA $DEBUG);
-$VERSION        = '0.025_000';
+$VERSION        = '0.025_001';
 $STRING_VERSION = $VERSION;
 ## no critic (BuiltinFunctions::ProhibitStringyEval)
 $VERSION = eval $VERSION;
--- xs/lib/Marpa/XS/Grammar.pm
+++ xs/lib/Marpa/XS/Grammar.pm
@@ -32,7 +32,7 @@ use integer;
 use utf8;
 
 use vars qw($VERSION $STRING_VERSION);
-$VERSION        = '0.025_000';
+$VERSION        = '0.025_001';
 $STRING_VERSION = $VERSION;
 ## no critic(BuiltinFunctions::ProhibitStringyEval)
 $VERSION = eval $VERSION;
